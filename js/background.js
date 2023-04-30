7e71348763573b1217e261a422ca589d45b59435
TheoK1m <123943297+TheoK1m@users.noreply.github.com>
2023-04-30 23:10:58 +0900

    20230430 commit

--- js/background.js
+++ js/background.js
@@ -1,9 +1,4 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -13,4 +8,4 @@ bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
 
-bgImage.classList.add("bg-style")
+bgImage.classList.add("bg-style");
--- js/greetings.js
+++ js/greetings.js
@@ -1,4 +1,3 @@
-
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -7,29 +6,28 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}, Welcome to my first JS page!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+  event.preventDefault();
+  console.dir(event);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGreetings(savedUsername);
+  paintGreetings(savedUsername);
 }
